6699b2bb3ee3ca5ea2a38164177783dd38ce2245
Elton Ortiz <elton.ortiz1@gmail.com>
2021-01-25 16:12:46 -0300

    projects-menu implementation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Header from './components/header/header.js'
 import Home   from './pages/home/home'
+import WorkProgress from './pages/workProgress/workProgress.js'
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import ProjectsMenu from './components/projects-menu/projects-menu.js';
 
 function App() {
- return(
-   <div>
+  return(
+    <div>
+     
+  <ProjectsMenu></ProjectsMenu>
   <Header></Header>
   <Switch>
       <Route path="/">
         <Home></Home>
+      </Route>
+      <Route path="/work">
+        <WorkProgress />
       </Route>
   </Switch>
    </div>
